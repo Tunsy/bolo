0561e347b82dc358c70f1601022e80a1ddb3432a
Yolie Saepung <yolienic@gmail.com>
2017-05-06 16:00:31 -0700

    Added the new create table

--- db/createtable.sql
+++ db/createtable.sql
@@ -1,27 +1,23 @@
-DROP TABLE IF EXISTS HobbyHasEquipment;
-DROP TABLE IF EXISTS Conversation;
-DROP TABLE IF EXISTS Transaction;
-DROP TABLE IF EXISTS Listing;
-DROP TABLE IF EXISTS Equipment;
-DROP TABLE IF EXISTS Hobby;
-DROP TABLE IF EXISTS Borrower;
+DROP TABLE IF EXISTS CustomerRentsBooking
+DROP TABLE IF EXISTS Booking;
+DROP TABLE IF EXISTS Availability;
+DROP TABLE IF EXISTS Room_Photo;
+DROP TABLE IF EXISTS Room;
+DROP TABLE IF EXISTS Customer;
 DROP TABLE IF EXISTS Owner;
 DROP TABLE IF EXISTS User;
 
 CREATE TABLE User (
 	`uid`				INT UNSIGNED NOT NULL,
-	`location` 			VARCHAR(100) NOT NULL,
     `password` 			VARCHAR(100) NOT NULL,
     `email` 			VARCHAR(100) NOT NULL,
-    `firstname` 		VARCHAR(100) NOT NULL,
-    `lastname`			VARCHAR(100) NOT NULL,
-    `security_question` VARCHAR(200) NOT NULL,
-    `birthday` 			DATE NOT NULL,
+    `first_name` 		VARCHAR(100) NOT NULL,
+    `last_name`			VARCHAR(100) NOT NULL,
     `rating` 			INT NULL,
 	PRIMARY KEY(`uid`)
 );
 
-CREATE TABLE Borrower (
+CREATE TABLE Customer (
 	`bid` 				INT UNSIGNED NOT NULL,
     PRIMARY KEY			(`bid`),
 	FOREIGN KEY 		(`bid`) REFERENCES User(`uid`) ON DELETE CASCADE
@@ -33,61 +29,50 @@ CREATE TABLE Owner (
     FOREIGN KEY 		(`oid`) REFERENCES User(`uid`) ON DELETE CASCADE
 );
 
-CREATE TABLE Hobby (
-	`hid` 				INT UNSIGNED NOT NULL,
-	`name` 				VARCHAR(100) NOT NULL,
-    `photo` 			VARCHAR(200) NOT NULL,
-    PRIMARY KEY			(`hid`)
+CREATE TABLE Room (
+	`rid`               INT UNSIGNED NOT NULL,
+    `oid`               INT UNSIGNED NOT NULL,
+    `name`              VARCHAR (200) NOT NULL,
+    `location`          VARCHAR (200) NOT NULL, -- Might use open-source thingie
+    `price`             DECIMAL (10,2) NOT NULL,
+    PRIMARY KEY			(`hid`),
+    FOREIGN KEY         (`oid`) REFERENCES Owner(`oid`) ON DELETE NO ACTION
 );
 
-CREATE TABLE Equipment (
-	`eid` 				INT UNSIGNED NOT NULL,
-    `photo` 			VARCHAR(200) NOT NULL,
-    `name`				VARCHAR(100) NOT NULL,
-    PRIMARY KEY			(`eid`)
+CREATE TABLE Room_Photo (
+    `rid`               INT UNSIGNED NOT NULL,
+    `photo_url`         VARCHAR(300) NOT NULL,
+    PRIMARY KEY         (`rid`),
+    FOREIGN KEY         (`rid`) REFERENCES Room(`rid`) ON DELETE NO ACTION
 );
 
-CREATE TABLE Listing (
-	`lid`				INT UNSIGNED NOT NULL,
-	`oid`				INT UNSIGNED NOT NULL,
-    `eid`				INT UNSIGNED NOT NULL,
-    `photo`				VARCHAR(200) NOT NULL,
-    `item_name`			VARCHAR(100) NOT NULL,
-    `price`				DECIMAL(10,2) NOT NULL,
-    `rating`			DECIMAL(2,2) NOT NULL, 
-    `condition`			INT NOT NULL, 
-    `availability`		BOOLEAN,	
-    `pickupOrDelivery`	VARCHAR(100) NOT NULL, 
-    PRIMARY KEY			(`lid`),
-    FOREIGN KEY			(`oid`) REFERENCES Owner(`oid`) ON DELETE CASCADE,
-    FOREIGN KEY			(`eid`) REFERENCES Equipment(`eid`) ON DELETE CASCADE
+CREATE TABLE Availability (
+    `rid`               INT UNSIGNED NOT NULL,
+    `start_datetime`    DATETIME NOT NULL,
+    `end_datetime`      DATETIME NOT NULL,
+    PRIMARY KEY         (`rid`),
+    FOREIGN KEY         (`rid`) REFERENCES Room(`rid`) ON DELETE NO ACTION
 );
 
-CREATE TABLE Conversation (
+CREATE TABLE Booking (
+	`bid`				INT UNSIGNED NOT NULL,
 	`cid`				INT UNSIGNED NOT NULL,
-    `bid`				INT UNSIGNED NOT NULL,
-    `lid`				INT UNSIGNED NOT NULL,
-    `datetime`			DATETIME NOT NULL,
-    PRIMARY KEY			(`cid`),
-    FOREIGN KEY			(`bid`) REFERENCES Borrower(`bid`) ON DELETE NO ACTION,
-    FOREIGN KEY			(`lid`) REFERENCES Listing(`lid`) ON DELETE NO ACTION
+    `rid`				INT UNSIGNED NOT NULL,
+    `grand_total_price`	DECIMAL(10,2) NOT NULL,
+    `subtotal_price`    DECIMAL(10,2) NOT NULL,
+    `start_datetime`    DATETIME NOT NULL,
+    `end_datetime`      DATETIME NOT NULL, 
+    PRIMARY KEY			(`bid`),
+    FOREIGN KEY			(`cid`) REFERENCES Customer(`cid`) ON DELETE CASCADE,
+    FOREIGN KEY			(`rid`) REFERENCES Room(`rid`) ON DELETE NO ACTION
 );
 
-CREATE TABLE Transaction (
-	`tid`				INT UNSIGNED NOT NULL,
-    `bid`				INT UNSIGNED NOT NULL,
-    `lid`				INT UNSIGNED NOT NULL,
-    `date`				DATE NOT NULL,
-    `numberOfDays`		INT NOT NULL,
-    PRIMARY KEY			(`tid`),
-    FOREIGN KEY			(`bid`) REFERENCES Borrower(`bid`) ON DELETE NO ACTION,
-    FOREIGN KEY			(`lid`) REFERENCES Listing(`lid`) ON DELETE NO ACTION
+CREATE TABLE CustomerRentsBooking (
+    `cid`               INT UNSIGNED NOT NULL,
+    `bid`               INT UNSIGNED NOT NULL,
+    PRIMARY KEY         (`bid`, `cid`),
+    FOREIGN KEY         (`bid`) REFERENCES Booking(`bid`),
+    FOREIGN KEY         (`cid`) REFERENCES Customer(`cid`)
 );
 
-CREATE TABLE HobbyHasEquipment (
-	`hid`				INT UNSIGNED NOT NULL,
-    `eid`				INT UNSIGNED NOT NULL,
-    PRIMARY KEY			(`hid`,`eid`),
-    FOREIGN KEY			(`hid`) REFERENCES Hobby(`hid`) ON DELETE CASCADE,
-    FOREIGN KEY			(`eid`) REFERENCES Equipment(`eid`) ON DELETE CASCADE
-);
+
