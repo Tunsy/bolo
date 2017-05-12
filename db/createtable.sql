e0053d585789451e7d1a7bed8fb005d7dfc67f58
Yolie Saepung <yolienic@gmail.com>
2017-05-11 20:04:03 -0700

    Fixed database again

--- db/createtable.sql
+++ db/createtable.sql
@@ -1,3 +1,4 @@
+use bolo;
 DROP TABLE IF EXISTS CustomerRentsBooking
 DROP TABLE IF EXISTS Booking;
 DROP TABLE IF EXISTS Availability;
@@ -8,29 +9,29 @@ DROP TABLE IF EXISTS Owner;
 DROP TABLE IF EXISTS User;
 
 CREATE TABLE User (
-	`uid`				INT UNSIGNED NOT NULL,
-    `password` 			VARCHAR(100) NOT NULL,
-    `email` 			VARCHAR(100) NOT NULL,
-    `first_name` 		VARCHAR(100) NOT NULL,
-    `last_name`			VARCHAR(100) NOT NULL,
-    `rating` 			INT NULL,
-	PRIMARY KEY(`uid`)
+    `uid`               INT UNSIGNED NOT NULL,
+    `password`          VARCHAR(100) NOT NULL,
+    `email`             VARCHAR(100) NOT NULL,
+    `first_name`        VARCHAR(100) NOT NULL,
+    `last_name`         VARCHAR(100) NOT NULL,
+    `rating`            INT NULL,
+    PRIMARY KEY(`uid`)
 );
 
 CREATE TABLE Customer (
-	`bid` 				INT UNSIGNED NOT NULL,
-    PRIMARY KEY			(`bid`),
-	FOREIGN KEY 		(`bid`) REFERENCES User(`uid`) ON DELETE CASCADE
+    `bid`               INT UNSIGNED NOT NULL,
+    PRIMARY KEY         (`bid`),
+    FOREIGN KEY         (`bid`) REFERENCES User(`uid`) ON DELETE CASCADE
 );
 
 CREATE TABLE Owner (
-	`oid` 				INT UNSIGNED NOT NULL,
-    PRIMARY KEY			(`oid`),
-    FOREIGN KEY 		(`oid`) REFERENCES User(`uid`) ON DELETE CASCADE
+    `oid`               INT UNSIGNED NOT NULL,
+    PRIMARY KEY         (`oid`),
+    FOREIGN KEY         (`oid`) REFERENCES User(`uid`) ON DELETE CASCADE
 );
 
 CREATE TABLE Room (
-	`rid`               INT UNSIGNED NOT NULL,
+    `rid`               INT UNSIGNED NOT NULL,
     `oid`               INT UNSIGNED NOT NULL,
     `name`              VARCHAR (200) NOT NULL,
     `location`          VARCHAR (200) NOT NULL, -- Might use open-source thingie
@@ -50,7 +51,7 @@ CREATE TABLE Room (
     `projector`         BOOLEAN,
     `speaker`           BOOLEAN,
     `fax_machine`       BOOLEAN,
-    PRIMARY KEY			(`hid`),
+    PRIMARY KEY         (`hid`),
     FOREIGN KEY         (`oid`) REFERENCES Owner(`oid`) ON DELETE NO ACTION
 );
 
@@ -70,16 +71,16 @@ CREATE TABLE Availability (
 );
 
 CREATE TABLE Booking (
-	`bid`				INT UNSIGNED NOT NULL,
-	`cid`				INT UNSIGNED NOT NULL,
-    `rid`				INT UNSIGNED NOT NULL,
-    `grand_total_price`	DECIMAL(10,2) NOT NULL,
+    `bid`               INT UNSIGNED NOT NULL,
+    `cid`               INT UNSIGNED NOT NULL,
+    `rid`               INT UNSIGNED NOT NULL,
+    `grand_total_price` DECIMAL(10,2) NOT NULL,
     `subtotal_price`    DECIMAL(10,2) NOT NULL,
     `start_datetime`    DATETIME NOT NULL,
     `end_datetime`      DATETIME NOT NULL, 
-    PRIMARY KEY			(`bid`),
-    FOREIGN KEY			(`cid`) REFERENCES Customer(`cid`) ON DELETE CASCADE,
-    FOREIGN KEY			(`rid`) REFERENCES Room(`rid`) ON DELETE NO ACTION
+    PRIMARY KEY         (`bid`),
+    FOREIGN KEY         (`cid`) REFERENCES Customer(`cid`) ON DELETE CASCADE,
+    FOREIGN KEY         (`rid`) REFERENCES Room(`rid`) ON DELETE NO ACTION
 );
 
 CREATE TABLE CustomerRentsBooking (
@@ -89,5 +90,4 @@ CREATE TABLE CustomerRentsBooking (
     FOREIGN KEY         (`bid`) REFERENCES Booking(`bid`),
     FOREIGN KEY         (`cid`) REFERENCES Customer(`cid`)
 );
-
 
