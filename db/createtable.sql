d3f7cd0f2232e881393c5b99c0b5e496331b2808
Jon Desiderio <desiderj@uci.edu>
2017-05-21 16:43:16 -0700

    fixed typo in Room.longitude

--- db/createtable.sql
+++ db/createtable.sql
@@ -53,7 +53,7 @@ CREATE TABLE Room (
     `fax_machine`       BOOLEAN,
     `rating`			DECIMAL(3,2) UNSIGNED NULL,
     `latitude`			DECIMAL(9,7) NOT NULL,
-    `longitude`			DECIMAL(11,7) NOT NULL,
+    `longitude`			DECIMAL(10,7) NOT NULL,
     PRIMARY KEY         (`rid`),
     FOREIGN KEY         (`oid`) REFERENCES Owner(`oid`) ON DELETE NO ACTION
 );
