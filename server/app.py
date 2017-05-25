860f27af82e056b7aa2c261ed04da6f1a8fcddc3
chriswongtv <chris@cogifire.com>
2017-05-24 22:55:08 -0700

    Some changes

--- server/app.py
+++ server/app.py
@@ -277,8 +277,7 @@ def get_user():
 	cursor.execute("SELECT * FROM User where uid='" + uid + "'")
 	data = cursor.fetchone()
 	# Return user info as JSON
-	return json.dumps({"email": data[2], "first_name": data[3],
-                           "last_name": data[4], "rating": data[5]})
+	return json.dumps({"email": data[2], "name": data[3] + ' ' + data[4], "rating": data[5]})
 
 @app.route('/api/getReservations')
 def get_reservations():
