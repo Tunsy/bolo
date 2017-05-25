37192ec51d2429d9a3eb2d56c57f39a83e849bc5
Johnny Nguyen <jonthnngyn@gmail.com>
2017-05-25 02:44:33 -0700

    Create registration page, login and registration validation, link buttons

--- server/static/js/script.js
+++ server/static/js/script.js
@@ -35,3 +35,82 @@ $("#past-button").click(function() {
     $('#pending').hide();
     $('#past').show();
 });
+
+$('#registration-form')
+  .form({
+    fields: {
+      firstname: {
+        identifier: 'first-name',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your first name'
+          }
+        ]
+      },
+      lastname: {
+        identifier: 'last-name',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your last name'
+          }
+        ]
+      },
+      email: {
+        identifier: 'email',
+        rules: [
+          {
+            type   : 'email',
+            prompt : 'Please enter a valid email'
+          }
+        ]
+      },
+      password: {
+        identifier: 'password',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a password'
+          },
+          {
+            type   : 'minLength[6]',
+            prompt : 'Your password must be at least {ruleValue} characters'
+          }
+        ]
+      },
+      terms: {
+        identifier: 'terms',
+        rules: [
+          {
+            type   : 'checked',
+            prompt : 'You must agree to the terms and conditions'
+          }
+        ]
+      }
+    }
+  })
+
+  $('#login-form')
+  .form({
+    fields: {
+      email: {
+        identifier: 'email',
+        rules: [
+          {
+            type   : 'email',
+            prompt : 'Please enter a valid email'
+          }
+        ]
+      },
+      password: {
+        identifier: 'password',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a password'
+          },
+        ]
+      },
+    }
+  })
